6863c57da4118ca1f794360d3e5e15d8e6025b8e
Sczygelski <avamariescz@gmail.com>
2022-10-03 20:32:34 -0500

    adding more routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,1 +1,24 @@
 const router = require('express').Router()
+const {
+    getNotes,
+    saveNote,
+    deleteNote
+} = requier('../../public/assets/js/index')
+const { notes } = require('../../db/db.json')
+
+router.get('/notes', (req,res) => {
+    let results = notes
+    if (req.query) {
+        results = filterByQuery(req.query,results)
+    }
+    res.json(results)
+})
+
+router.post('/notes', (req,res) => {
+    req.body.id = notes.length.toString();
+    res.status(400).send('The note is not properly formatted.')
+})
+
+router.delete('/notes', (req,res) => {
+
+})
