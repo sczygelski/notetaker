70a037398041343ed964ab1e54d28729b7ac6cd9
Sczygelski <avamariescz@gmail.com>
2022-10-03 19:51:17 -0500

    finished server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const { response } = require('express')
+const express = require('express')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+const notesRoutes = require('./routes/apiRoutes/notesRoutes')
+const htmlRoutes = require('./routes/htmlRoutes/index')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/api', notesRoutes)
+app.use('/', htmlRoutes)
+
+app.listen(PORT, () => {
+    console.log(`API server is now on port ${PORT}!`)
+})
